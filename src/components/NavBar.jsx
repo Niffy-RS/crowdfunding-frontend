4ff9941e02b5da915e9a3416ad61ac2d6ad3b409
jennifer <jennifer_rs16@hotmail.com>
2026-01-31 12:14:45 +1000

    added about and contact page - updated navbar and router

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,6 +5,8 @@ function NavBar() {
     <div>
       <nav>
         <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
       </nav>
       <Outlet />
     </div>
